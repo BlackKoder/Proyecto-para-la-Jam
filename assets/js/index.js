ec044d443ddd9fd05f73109bbc5ca55954fa1e86
BlackKoder <chrysxdzer0@gmail.com>
2023-04-15 17:44:29 -0430

    Primera versión de Master Quest

---
+++ assets/js/index.js
@@ -0,0 +1,265 @@
+//Esto hace el llamado a nuestro canvas
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+//Aquí establecemos nuestras dimensiones para nuestro canvas
+canvas.width = 1024;
+canvas.height = 576;
+
+//Este va a ser el valor para la gravedad
+const gravity = 0.5;
+
+
+//Esta clase es para el jugador, aquí está la posición velocidad etc.
+//NOTA: Si no puedo pasar las propiedades del constructor como un objetos, es porque
+//No las he definido como un argumento
+class Sprite{
+	constructor({ position, velocity, color = 'red', offset }){
+		this.position = position;
+		//Aquí se modifica la velocidad
+		this.velocity = velocity;
+		//Aquí el ancho de las cajas rojas de los personajes
+		this.width = 50;
+		//Aquí se modifica la altura del cuadro rojo del personaje
+		this.height = 150;
+		//Este es para los controles
+		this.lastKey;
+		//Este es para el cuadro de ataque
+		this.attackBox = {
+			position: {
+				x: this.position.x,
+				y: this.position.y
+			},
+			offset,
+			width: 100,
+			height: 50
+		}
+		this.color = color;
+		this.isAttacking;
+
+	}
+
+	//Aquí se dibuja el cuadro rojo, se establece sus coordenadas y dimensiones
+	draw(){
+		c.fillStyle = this.color;
+		c.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+		//Caja de ataque
+		if (this.isAttacking) {
+			c.fillStyle = "green";
+			c.fillRect(
+				this.attackBox.position.x, 
+				this.attackBox.position.y, 
+				this.attackBox.width, 
+				this.attackBox.height
+				);	
+		}
+	}
+
+	//Aquí se actualiza todos los valores de draw position y velocity
+	update(){
+		this.draw();
+		this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+		this.attackBox.position.y = this.position.y;
+
+		this.position.x += this.velocity.x;
+		this.position.y += this.velocity.y;
+
+		if (this.position.y + this.height + this.velocity.y < canvas.height) {
+			this.velocity.y += gravity;
+		}else {
+			this.velocity.y = 0;
+		}
+	}
+
+	attack(){
+		this.isAttacking = true;
+		setTimeout(() => {
+			this.isAttacking = false;
+		}, 100)
+	}
+}
+
+
+//Aquí puedo alterar la posición del jugador en el eje x y 
+const player = new Sprite({
+			position:{
+				x: 0,
+				y: 0
+			},
+			velocity:{
+				x: 0,
+				y: 1
+			},
+			offset:{
+				x: 0,
+				y: 0
+			}
+			
+		});
+const enemy = new Sprite({
+			position:{
+				x: 300,
+				y: 100
+			},
+			velocity:{
+				x: 0,
+				y: 1
+			},
+			offset:{
+				x: -50,
+				y: 0
+			},
+			color: 'blue'
+		});
+
+//Este es el valor por defecto de nuestras teclas de dirección
+
+const keys = {
+	d:{
+		pressed: false,
+	},
+	a:{
+		pressed: false,
+	},
+	ArrowRight:{
+		pressed: false,
+	},
+	ArrowLeft:{
+		pressed: false,
+	}
+}
+
+function rectangularCollision({ rectangle1, rectangle2}){
+	return(rectangle1.attackBox.position.x  + rectangle1.attackBox.width >=
+				rectangle2.position.x && 
+			rectangle1.attackBox.position.x <= 
+				rectangle2.position.x + rectangle2.width && 
+			rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+				rectangle2.position.y && 
+			rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+		)
+}
+
+//Esta función lo que hace es dibujar en nuestro liezo varios cuadros por segundo
+//Se mantiene activo todo el tiempo
+function animate(){
+	window.requestAnimationFrame(animate);
+	c.fillStyle = "white";
+	c.fillRect(0, 0, canvas.width, canvas.height);
+	player.update();
+	enemy.update();
+
+
+	//Aquí altero la velocidad del personaje y el desplazamiento del jugador
+	player.velocity.x = 0;
+	enemy.velocity.x = 0;
+
+	if (keys.d.pressed && player.lastKey === 'd') {
+		player.velocity.x = 5;
+	}else if(keys.a.pressed && player.lastKey === 'a'){
+		player.velocity.x = -5;
+	}
+
+	//Aquí altero la velocidad del personaje y el desplazamiento del enemigo
+
+	if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+		enemy.velocity.x = 5;
+	}else if(keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft'){
+		enemy.velocity.x = -5;
+	}
+
+	//Detección de colisiones
+	if (
+		rectangularCollision({
+			rectangle1: player,
+			rectangle2: enemy
+		}) && 
+		player.isAttacking
+		) {
+		player.isAttacking = false;
+		console.log("Ataque");
+	}
+
+	if (
+		rectangularCollision({
+			rectangle1: enemy,
+			rectangle2: player
+		}) && 
+		enemy.isAttacking
+		) {
+		enemy.isAttacking = false;
+		console.log("Daño");
+	}
+
+}
+
+animate();
+
+
+//Aquí están los controles, se mantiene en escucha en un evento por si presiono una
+//tecla en el teclado
+window.addEventListener('keydown', (event) =>{
+	switch(event.key){
+		case 'd':
+			keys.d.pressed = true;
+			player.lastKey = 'd';
+		break;	
+
+		case 'a':
+			keys.a.pressed = true;
+			player.lastKey = 'a';
+		break; 
+
+		case 'w':
+			player.velocity.y = -20;
+		break; 
+
+		case 'k':
+			player.attack();
+		break;
+
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = true;
+			enemy.lastKey = 'ArrowRight';
+		break;	
+
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = true;
+			enemy.lastKey = 'ArrowLeft';
+		break; 
+
+		case 'ArrowUp':
+			enemy.velocity.y = -20;
+		break; 
+
+		case 'p':
+			enemy.isAttacking = true;
+		break;
+	}
+});
+
+//Este es el contrario de la dirección a donde establecimos que vaya el personje
+//Para que se mantenga en un solo sitio y no se mueva a lo loco
+
+window.addEventListener('keyup', (event) =>{
+	switch(event.key){
+		case 'd':
+			keys.d.pressed = false;
+		break;	
+
+		case 'a':
+			keys.a.pressed = false;
+		break; 
+	}
+
+	switch(event.key){
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = false;
+		break;
+		
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = false;
+		break;		
+	}
+});
